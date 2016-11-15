d1e0b9219efb9f09a212405387acbcced2b3c896
milansegedinac <milan.segedinac@gmail.com>
2016-11-15 18:20:54 +0100

    Lab 06 - domaci

--- lab6-end/src/main/webapp/static/app/js/main.js
+++ lab6-end/src/main/webapp/static/app/js/main.js
@@ -4,6 +4,12 @@ app.config(function($routeProvider) {
 	$routeProvider.when('/home',{
 		templateUrl:'/static/app/html/partials/home.html'
 	});
+	$routeProvider.when('/domaci',{
+		templateUrl:'/static/app/html/partials/domaci.html'
+	});
+	$routeProvider.otherwise({
+            redirectTo: '/domaci'
+        });
 });
 app.controller('myCtrl', 
 	function ($scope) {
@@ -15,3 +21,15 @@ app.controller('myCtrl',
 		}
 	}
 );
+app.controller('zadatak1',function ($scope) {
+	$scope.tekst='';
+	$scope.ponisti = function(){
+		$scope.tekst='';
+	}
+});
+
+app.controller('zadatak2', function($scope){
+	$scope.prebaci = function(){
+		$scope.polje1 = $scope.polje2;
+	}
+});
